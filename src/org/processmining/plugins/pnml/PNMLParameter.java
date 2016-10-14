d85ce9a1bc32a650e9e70cc39c379f6a48bad06a
bfvdonge <bfvdonge@a68f48d4-8d00-424f-b812-3f22a534a02a>
2016-10-14 16:38:34 +0000

    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/StochasticPetriNets/Trunk@31093 a68f48d4-8d00-424f-b812-3f22a534a02a

--- src/org/processmining/plugins/pnml/PNMLParameter.java
+++ src/org/processmining/plugins/pnml/PNMLParameter.java
@@ -1,17 +1,24 @@
 package org.processmining.plugins.pnml;
 
-import org.processmining.plugins.pnml.graphics.PnmlPosition;
+import org.processmining.plugins.pnml.elements.graphics.PnmlPosition;
 
 /**
- * The default way of Prom to import PNML files is to scale them by a factor of {@value PnmlPosition#SCALE} (see {@link PnmlPosition#SCALE}) 
- * We do not want to change this, so that we are compatible with importing and exporting PNML files
+ * The default way of Prom to import PNML files is to scale them by a factor of
+ * {@value PnmlPosition#SCALE} (see {@link PnmlPosition#SCALE}) We do not want
+ * to change this, so that we are compatible with importing and exporting PNML
+ * files
  * 
  * @author Andreas Rogge-Solti
- *
+ * 
  */
 public class PNMLParameter {
 
-	public static double getScaleForViewInProM(){
-		return PnmlPosition.SCALE;
+	/**
+	 * BVD: Added local constant here as the other constant is no longer visible
+	 */
+	private static final double SCALE = 2.0;
+
+	public static double getScaleForViewInProM() {
+		return SCALE;
 	}
 }
