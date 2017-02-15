02cd1b8bfa3c762cce44d47407eaeb093f23b9f8
arogge <arogge@a68f48d4-8d00-424f-b812-3f22a534a02a>
2017-02-15 16:40:48 +0000

    utility class to preprocess WABO logs
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/StochasticPetriNets/Trunk@34055 a68f48d4-8d00-424f-b812-3f22a534a02a

---
+++ src/org/processmining/plugins/filter/context/PreprocessLogFilter.java
@@ -0,0 +1,97 @@
+package org.processmining.plugins.filter.context;
+
+import org.deckfour.xes.classification.XEventClass;
+import org.deckfour.xes.classification.XEventClasses;
+import org.deckfour.xes.classification.XEventNameClassifier;
+import org.deckfour.xes.extension.std.XConceptExtension;
+import org.deckfour.xes.extension.std.XTimeExtension;
+import org.deckfour.xes.factory.XFactoryRegistry;
+import org.deckfour.xes.info.XLogInfo;
+import org.deckfour.xes.model.XAttributeMap;
+import org.deckfour.xes.model.XEvent;
+import org.deckfour.xes.model.XLog;
+import org.deckfour.xes.model.XTrace;
+
+import java.util.*;
+
+/**
+ * Created by andreas on 2/15/17.
+ */
+public class PreprocessLogFilter {
+    public static XLog renameUnderscores(XLog input) {
+        String fromPattern = "_";
+        String toPattern = "$";
+
+        XLog log = XFactoryRegistry.instance().currentDefault().createLog((XAttributeMap) input.getAttributes().clone());
+
+        for (XTrace trace : input) {
+            XTrace newTrace = XFactoryRegistry.instance().currentDefault().createTrace((XAttributeMap) trace.getAttributes().clone());
+            for (XEvent e : trace) {
+                XEvent newEvent = XFactoryRegistry.instance().currentDefault().createEvent((XAttributeMap) e.getAttributes().clone());
+                String newName = XConceptExtension.instance().extractName(e).replaceAll(fromPattern,toPattern);
+                XConceptExtension.instance().assignName(newEvent, newName);
+                newTrace.add(newEvent);
+            }
+            log.add(newTrace);
+        }
+        return log;
+    }
+
+    public static XLog reDuplicateEvents(XLog input){
+        String splitPattern = "_";
+
+        XLog log = XFactoryRegistry.instance().currentDefault().createLog((XAttributeMap) input.getAttributes().clone());
+        XLogInfo info = input.getInfo(new XEventNameClassifier());
+        XEventClasses classes = info.getEventClasses();
+        Set<String> eventNames = new HashSet<>();
+        for (XEventClass eventClass : classes.getClasses()){
+            eventNames.add(eventClass.getId());
+        }
+        Map<String, String> renameMap = new HashMap<>();
+        for (XEventClass eventClass : classes.getClasses()){
+            String[] parts = eventClass.getId().split(splitPattern);
+            try {
+                Integer num = Integer.valueOf(parts[parts.length - 1]);
+                String neighborClass = eventClass.getId().substring(0, eventClass.getId().lastIndexOf(splitPattern));
+                if (num == 0 && eventNames.contains(neighborClass+splitPattern+"1")
+                        || num > 0 && eventNames.contains(neighborClass+splitPattern+(num-1))){
+                    renameMap.put(eventClass.getId(), neighborClass);
+                }
+            } catch (NumberFormatException e){
+            }
+        }
+
+        for (XTrace trace : input) {
+            XTrace newTrace = XFactoryRegistry.instance().currentDefault().createTrace((XAttributeMap) trace.getAttributes().clone());
+            for (XEvent e : trace) {
+                XEvent newEvent = XFactoryRegistry.instance().currentDefault().createEvent((XAttributeMap) e.getAttributes().clone());
+                String oldName = XConceptExtension.instance().extractName(e);
+                String newName = renameMap.containsKey(oldName) ? renameMap.get(oldName) : oldName;
+                XConceptExtension.instance().assignName(newEvent, newName);
+                newTrace.add(newEvent);
+            }
+            log.add(newTrace);
+        }
+        return log;
+    }
+
+    public static XLog reorderByTime(XLog input) {
+        XLog log = XFactoryRegistry.instance().currentDefault().createLog((XAttributeMap) input.getAttributes().clone());
+
+        for (XTrace trace : input) {
+            XTrace newTrace = XFactoryRegistry.instance().currentDefault().createTrace((XAttributeMap) trace.getAttributes().clone());
+            for (XEvent e : trace) {
+                XEvent newEvent = XFactoryRegistry.instance().currentDefault().createEvent((XAttributeMap) e.getAttributes().clone());
+                newTrace.add(newEvent);
+            }
+            Collections.sort(newTrace, new Comparator<XEvent>() {
+                @Override
+                public int compare(XEvent o1, XEvent o2) {
+                    return XTimeExtension.instance().extractTimestamp(o1).compareTo(XTimeExtension.instance().extractTimestamp(o2));
+                }
+            });
+            log.add(newTrace);
+        }
+        return log;
+    }
+}
