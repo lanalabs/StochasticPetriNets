b29367b3ae10d3b3f31bbe3450dd8e761099bc96
arogge <arogge@a68f48d4-8d00-424f-b812-3f22a534a02a>
2017-06-07 20:52:59 +0000

    prepare quantile based estimation
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/StochasticPetriNets/Trunk@35773 a68f48d4-8d00-424f-b812-3f22a534a02a

---
+++ src/org/processmining/plugins/stochasticpetrinet/measures/entropy/EntropyCalculatorQuantile.java
@@ -0,0 +1,30 @@
+package org.processmining.plugins.stochasticpetrinet.measures.entropy;
+
+/**
+ *
+ * Created by andreas on 6/7/17.
+ */
+public class EntropyCalculatorQuantile extends EntropyCalculatorExact {
+
+    private final double quantile;
+
+    /**
+     * Constructs a calculator that measures the entropy of a model's possible runs up to a given quantile o
+     * @param quantile
+     */
+    public EntropyCalculatorQuantile(double quantile) {
+        super();
+        if (quantile < 0 || quantile > 1){
+            throw new IllegalArgumentException("Confidence must be between 0 and 1!");
+        }
+        this.quantile = quantile;
+    }
+
+    public String getMeasureName() {
+        return "Model entropy measure (using "+Math.round(this.quantile*100)+"% quantile of states)";
+    }
+
+    protected String getNameInfo() {
+        return "quantile";
+    }
+}
