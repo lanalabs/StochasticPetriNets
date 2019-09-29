153a8c0acb126480f11b10c470bb80d4ab502236
arogge <arogge@a68f48d4-8d00-424f-b812-3f22a534a02a>
2019-09-29 21:23:33 +0000

    added missing option to add graphics to text elements.
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/StochasticPetriNets/Trunk@42552 a68f48d4-8d00-424f-b812-3f22a534a02a

--- src/org/processmining/plugins/pnml/simple/PNMLText.java
+++ src/org/processmining/plugins/pnml/simple/PNMLText.java
@@ -6,6 +6,9 @@ import org.simpleframework.xml.Root;
 @Root
 public class PNMLText {
 
+    @Element(name = "graphics", required = false)
+    protected PNMLGraphics graphics;
+
     @Element(name = "text", required = false)
     private String text;
 
@@ -22,5 +25,13 @@ public class PNMLText {
 
     public void setText(String text) {
         this.text = text;
+    }
+
+    public PNMLGraphics getGraphics() {
+        return graphics;
+    }
+
+    public void setGraphics(PNMLGraphics graphics) {
+        this.graphics = graphics;
     }
 }
